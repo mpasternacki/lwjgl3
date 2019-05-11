ea171f8224e6a06b591bdf7c3175cb0b241ef001
Johannes Lundberg <johalun0@gmail.com>
2019-05-10 19:53:20 -0700

    Replace alloca.h with stdlib.h

--- modules/lwjgl/stb/src/templates/kotlin/stb/templates/stb_vorbis.kt
+++ modules/lwjgl/stb/src/templates/kotlin/stb/templates/stb_vorbis.kt
@@ -11,7 +11,7 @@ import stb.*
 val _stb_vorbis = "STBVorbis".nativeClass(Module.STB, prefixMethod = "stb_vorbis_", prefixConstant = "VORBIS_", library = STB_LIBRARY) {
     includeSTBAPI(
         """#ifdef LWJGL_LINUX
-    #include <alloca.h>
+    #include <stdlib.h>
 #endif
 #include "stb_vorbis.c"""")
 
